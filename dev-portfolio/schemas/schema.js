d4c36175e931de10cfa02e8e7ec5ef9d716eb8d8
RACHIT PATEL <80671047+ra463@users.noreply.github.com>
2022-07-07 18:25:31 +0530

    Update schema.js
    removed useless code

--- dev-portfolio/schemas/schema.js
+++ dev-portfolio/schemas/schema.js
@@ -16,6 +16,7 @@ export default createSchema({
   name: "default",
   // Then proceed to concatenate our document type
   // to the ones provided by any plugins that are installed
+  
   types: schemaTypes.concat([
     works,
     certificate,
